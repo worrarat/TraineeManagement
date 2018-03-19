7a95ca98403668a84b241c503a5e579f4109c5f2
Worrarat.j <worrarat.j@pkru.ac.th>
2018-03-19 11:39:24 +0700

    update

--- main/login.php
+++ main/login.php
@@ -1,20 +1,31 @@
 <?php
     session_start();
     include("dbconfig.php");
-    $sql = 'SELECT * FROM staff_tb WHERE stf_user ='."'$_POST[id]'"." and stf_pass = "."'$_POST[pass]'";
-    if($res = $DB_con->query($sql)){
+    //staff login
+    $sql_staff = 'SELECT * FROM staff_tb WHERE stf_user ='."'$_POST[id]'"." and stf_pass = "."'$_POST[pass]'";
+    if($res = $DB_con->query($sql_staff )){
       if ($res->fetchColumn() > 0){
-        foreach ($DB_con->query($sql) as $row){
+        foreach ($DB_con->query($sql_staff) as $row){
               $_SESSION['name'] = $row['stf_name'];
               $_SESSION['surname'] = $row['stf_surname'];
-              if($_SESSION['name'] == '' of $_SESSION['surname'] =='')
-                  header("Location: /TraineeManagement/index-fail.html");
+              $status = $row['login_status'];
+              if($status == "1")
+              {
+                  echo "exist status";
+                  exit();
+              }
               else
+              {
                   header("Location: /TraineeManagement/main/blank.html");
+                  exit();
+              }
+
         }
       }
-      else {
+      else
+      {
               header("Location: /TraineeManagement/index-fail.html");
+              exit();
       }
     }
 ?>
