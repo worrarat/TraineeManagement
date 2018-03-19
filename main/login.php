7bd983184f2107d7ae932ac05b023a732d2ca8dc
Worrarat.j <worrarat.j@pkru.ac.th>
2018-03-19 09:26:35 +0700

    update

--- main/login.php
+++ main/login.php
@@ -1,12 +1,16 @@
 <?php
+    session_start();
     include("dbconfig.php");
     $sql = 'SELECT * FROM staff_tb WHERE stf_user ='."'$_POST[id]'"." and stf_pass = "."'$_POST[pass]'";
     if($res = $DB_con->query($sql)){
       if ($res->fetchColumn() > 0){
         foreach ($DB_con->query($sql) as $row){
-              print $row['stf_name'] . "&nbsp";
-              print $row['stf_surname'] . "</br>";
-              header("Location: /TraineeManagement/main/blank.html");
+              $_SESSION['name'] = $row['stf_name'];
+              $_SESSION['surname'] = $row['stf_surname'];
+              if($_SESSION['name'] == '' of $_SESSION['surname'] =='')
+                  header("Location: /TraineeManagement/index-fail.html");
+              else
+                  header("Location: /TraineeManagement/main/blank.html");
         }
       }
       else {
